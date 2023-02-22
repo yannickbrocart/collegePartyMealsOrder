63c3dccf0cf39610507a2e9115243432463df4bb
yannickbrocart <mail@yannickbrocart.com>
2023-02-22 16:48:19 +0100

    Fourth commit

--- collegePartyMealsOrderTreatment.js
+++ collegePartyMealsOrderTreatment.js
@@ -18,6 +18,18 @@ class FormValidator {
         }
     }
 
+    validateOnSubmit() {
+        let self = this;
+        this.form.addEventListener('submit', function() {
+            event.preventDefault();
+            this.fields.forEach(function (field) {
+                field.addEventListener('input', function() {
+                    self.validateFields(field);
+                });
+            });
+        });
+    }
+
     validateFields(field) {
         if (field.value.trim() === "") {
             this.setStatus(field, `Le champs ${field.previousElementSibling.innerText} ne peut être vide`, "error");
@@ -25,12 +37,30 @@ class FormValidator {
             this.setStatus(field, null, "success");
         }
 
-        if (field.type === "email") {
-            const regex = /\S+@\S+\.\S+/
+        if (field.type === "text") {
+            const regex = /[a-zA-Z-_ ]{2,80}$/;
             if (regex.test(field.value)) {
-
+                this.setStatus(field, null, "success");
             } else {
-                console.log('no');
+                this.setStatus(field, null, "error");
+            }
+        }
+
+        if (field.type === "tel") {
+            const regex = /0[1-79]{1}\s\d{2}\s\d{2}\s\d{2}\s\d{2}$/;
+            if (regex.test(field.value)) {
+                this.setStatus(field, null, "success");
+            } else {
+                this.setStatus(field, null, "error");
+            }
+        }
+
+        if (field.type === "email") {
+            const regex = /\S+@\S+\.\S{2,6}$/;
+            if (regex.test(field.value)) {
+                this.setStatus(field, null, "success");
+            } else {
+                this.setStatus(field, null, "error");
             }
         }
     }
@@ -38,18 +68,17 @@ class FormValidator {
     setStatus(field, message, status) {
         const successIcon = field.parentElement.querySelector('.icon-success');
         const errorIcon = field.parentElement.querySelector('.icon-error');
-        const errorMessage = field.parentElement.querySelector('.error-message');
         if (status === "success") {
             if (errorIcon) { errorIcon.classList.add('hidden') }
-            // if (errorMessage) { errorMessage.innerText = "" }
-            successIcon.classList.remove('hidden')
-            field.classList.remove('input-error')
+            field.nextElementSibling.classList.remove('error-message');
+            successIcon.classList.remove('hidden');
+            field.classList.remove('input-error');
         }
         if (status === "error") {
             if (successIcon) { successIcon.classList.add('hidden') }
-            // field.parentElement.querySelector('.error-message').innerText = message
-            errorIcon.classList.remove('hidden')
-            field.classList.add('input-error')
+            field.nextElementSibling.classList.add('error-message');
+            errorIcon.classList.remove('hidden');
+            field.classList.add('input-error');
         }
     }
 }
@@ -70,3 +99,4 @@ const fields = document.querySelectorAll('input');
 
 const identityForm = new FormValidator(form, fields);
 identityForm.validateOnEntry();
+// identityForm.validateOnSubmit();
